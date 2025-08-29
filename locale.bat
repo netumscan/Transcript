c9a1d40d6965a64bd26566758c937c9cebc87ee6
ScanToys <forack@outlook.com>
2025-08-29 08:59:28 +0800

    初始化添加内容

---
+++ locale.bat
@@ -0,0 +1,29 @@
+cd ../Manual/Platform
+sphinx-build -b gettext source ../../Translate/Platform
+
+cd ../Reader
+sphinx-build -b gettext source ../../Translate/Reader
+
+cd ../Wired-212
+sphinx-build -b gettext source ../../Translate/Wired-212
+
+cd ../Wired-280
+sphinx-build -b gettext source ../../Translate/Wired-280
+
+cd ../Wired-290
+sphinx-build -b gettext source ../../Translate/Wired-290
+
+cd ../Wired-CCD
+sphinx-build -b gettext source ../../Translate/Wired-CCD
+
+cd ../Wired-DP
+sphinx-build -b gettext source ../../Translate/Wired-DP
+
+cd ../Wired-HS7
+sphinx-build -b gettext source ../../Translate/Wired-HS7
+
+cd ../Wireless
+sphinx-build -b gettext source ../../Translate/Wireless
+
+cd ../Wireless-WiFi
+sphinx-build -b gettext source ../../Translate/Wireless-WiFi
